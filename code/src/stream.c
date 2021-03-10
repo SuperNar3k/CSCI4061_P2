93627e04f987a8cab549faf9e66267fd9b2775e1
Narek Ohanyan <ohanyanarek@gmail.com>
2021-03-10 16:21:14 -0600

    words i guess

--- code/src/stream.c
+++ code/src/stream.c
@@ -1,15 +1,15 @@
 #include "stream.h"
 
 /**
- * read lines from input file and write each line to pipes
- * each line will contain words as in Project 1 (Use STDOUT for writing)
+ * Read lines from input file and write each line to pipes.
+ * Each line will contain words as in Project 1 (Use STDOUT for writing).
  */
 void emit(char * inputFileName) {
 
 }
 /***
  *
- * Stream process will read from the files created by Master
+ * Stream process will read from the files created by Master.
  */
 int main(int argc, char *argv[]) {
 
@@ -19,9 +19,10 @@ int main(int argc, char *argv[]) {
     char* ipFdr = "MapperInput/Mapper";
 
 
-    //TODO: Read lines from Mapper files and get the file names that will be processes in emit function
-    //Each MapperInput/MapperID.txt file will contain file names
-    // example of one line will be test/T1/subfolder/0.txt
+   /* TODO: Read lines from Mapper files and get the file names that will be processes in emit function.
+    * Each MapperInput/MapperID.txt file will contain file names.
+    * example of one line will be test/T1/subfolder/0.txt.
+    */
     while(){
         emit(filePath);
     }
